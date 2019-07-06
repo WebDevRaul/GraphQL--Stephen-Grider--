6783413a83884865f31d2f16d7c70807696a569e
TextError <savinraul1983@yahoo.com>
2019-07-06 18:42:29 +0100

    refeching queries

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -16,9 +17,8 @@ class SongCreate extends Component {
     const { title } = this.state;
 
     this.props.mutate({
-      variables: {
-        title
-      }
+      variables: { title },
+      refetchQueries: [{ query }]
     })
     .then(() => hashHistory.push('/'));
     
--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
 
@@ -29,14 +29,5 @@ class SongList extends Component {
     )
   }
 }
-
-const query = gql`
-  {
-    songs {
-      title
-      id
-    }
-  }
-`;
 
 export default graphql(query)(SongList);
