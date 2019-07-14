2328e414e012be623323f3ad595695e93ec592c5
TextError <savinraul1983@yahoo.com>
2019-07-14 16:46:11 +0100

    fetch likes and litle css

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -5,18 +5,21 @@ import { graphql } from 'react-apollo';
 class LyricList extends Component {
 
   onLike(id) {
-    console.log(id)
+    this.props.mutate({ variables: { id } });
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className='collection-item'>
           {content}
-          <i 
-            className='material-icons'
-            onClick={() => this.onLike(id)}
-          >thumb_up</i>
+          <div className='vote-box'>
+            <i 
+              className='material-icons'
+              onClick={() => this.onLike(id)}
+            >thumb_up</i>
+            {likes}
+          </div>
         </li>
       )
     })
