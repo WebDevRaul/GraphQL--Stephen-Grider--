c11877cb399f2ecb6d80fe2269d983a8c01af6e3
TextError <savinraul1983@yahoo.com>
2019-07-14 16:33:30 +0100

    mutation likeLyric

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
+
+  onLike(id) {
+    console.log(id)
+  }
 
   renderLyrics() {
     return this.props.lyrics.map(({ id, content }) => {
       return (
         <li key={id} className='collection-item'>
           {content}
+          <i 
+            className='material-icons'
+            onClick={() => this.onLike(id)}
+          >thumb_up</i>
         </li>
       )
     })
@@ -20,4 +30,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList)
